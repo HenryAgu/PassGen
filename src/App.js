d6dc122ad2c7b2ffa6ed8e05b4a70062d6d014f6
Henry <aguhenrychuks@gmail.com>
2022-10-31 12:56:49 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 // Stylesheet
-import './App.css'
+import "./App.css";
 
-// Components
-import Card from './components/Card';
 function App() {
   return (
     <>
-    <Card/>
+      {/* Pop up section */}
+      <div className="pop-up">
+        <div className="inner-pop">
+          <p>Copied!</p>
+          <i class="fa-sharp fa-solid fa-xmark"></i>
+        </div>
+      </div>
+      {/* Card section */}
+      <div className="card">
+        <div className="inner-cards">
+          <div className="password">
+            <span>Create Password</span>
+            <button>
+              <i class="fa-solid fa-clone"></i>
+            </button>
+          </div>
+          <div className="generate">
+            <button>Generate Password</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
