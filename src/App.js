6a5a6153ac2580ec836ae49b0e5b99aa5a56da78
Henry <aguhenrychuks@gmail.com>
2022-10-31 13:44:50 +0100

    added AOS animation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 // Stylesheet
 import "./App.css";
 
+// For AOS Animation
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// hooks
+import { useState, useEffect } from "react";
 function App() {
+  const [password, setPassword] = useState("Create Password");
+  const [popUp, setPopUp] = useState(false);
+
+  // Hide PopUp
+  const removePopUpHandler = () => {
+    setPopUp(false);
+  };
+
+  // Copy Password
+  const copyPasswordHandler = () => {
+    setPopUp(true);
+  };
+
+  // Generate Password
+  const passwordHandler = () => {};
+
+  // For AOS animation
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       {/* Pop up section */}
       <div className="pop-up">
-        <div className="inner-pop">
-          <p>Copied!</p>
-          <i class="fa-sharp fa-solid fa-xmark"></i>
-        </div>
+        {popUp ? (
+          <div className="inner-pop">
+            <p>Copied!</p>
+            <i
+              class="fa-sharp fa-solid fa-xmark"
+              onClick={removePopUpHandler}
+            ></i>
+          </div>
+        ) : null}
       </div>
       {/* Card section */}
       <div className="card">
         <div className="inner-cards">
           <div className="password">
-            <span>Create Password</span>
+            <span>{password}</span>
             <button>
-              <i class="fa-solid fa-clone"></i>
+              <i class="fa-solid fa-clone" onClick={copyPasswordHandler}></i>
             </button>
           </div>
           <div className="generate">
-            <button>Generate Password</button>
+            <button onClick={passwordHandler}>Generate Password</button>
           </div>
         </div>
       </div>
